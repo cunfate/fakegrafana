dc78bcbc3c162b0bac3324da6c2e64221848d265
zhangcun <cun.zhang@outlook.com>
2017-12-03 23:36:07 -0800

    migrate hinocchart to react

--- app/static/js/main/chartviews.js
+++ app/static/js/main/chartviews.js
@@ -1,4 +1,55 @@
-$(document).ready(function(){
+class HinocChartReact extends React.Component {
+    constructor(...args) {
+        super(...args);
+    }
+
+    render() {
+        let self = this;
+        return (
+            <div className="col-md-12" style={{height:420}}>
+                <div className="col-md-12" ref={function(ele){self._echartBar = ele;}} style={{height:20}}>
+                    <button type="button" className="close" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div className="col-md-12" ref={function(ele){self._echartContainer = ele;}} style={{height:400}}>
+                </div>
+            </div> 
+        );
+    }
+
+    componentDidMount() {
+        this._chart = echarts.init(this._echartContainer);
+        this._chartoption = {
+            title: {
+                text: 'New Hinoc chart'
+            },
+            tooltip:{},
+            backgroundColor: 'rgba(153, 255, 204, 0.3)',
+            legend: {
+                data:'show'
+            },
+            xAxis:{
+                data:[]
+            },
+            yAxis:{},
+            series: {
+                name: 'value',
+                type: 'line',
+                areaStyle:{normal:{}},
+                data:[]
+            }
+        };
+        this.showChart();
+    }
+
+    showChart() {
+        this._chart.setOption(this._chartoption);
+    }
+}
+
+window.addEventListener("load",function(){
+    alert("setup over");
     var testchart = echarts.init(document.getElementsByClassName("testchart1")[0]);
     var option = {
         title: {
@@ -21,28 +72,33 @@ $(document).ready(function(){
     };
     testchart.setOption(option);
 
-    $("#add-row").click(function() {
-        $("#row-adder-bar")[0].style.display="block";
-        $("#add-row")[0].style.display="none";
-    });
+    document.getElementById("add-row").onclick = function() {
+        document.getElementById("row-adder-bar").style.display="block";
+        document.getElementById("add-row").style.display="none";
+    };
 
-    $("#row-adder-bar").click(function() {
-        $("#row-adder-bar")[0].style.display="none";
-        $("#add-row")[0].style.display="block";
-    });
+    document.getElementById("row-adder-bar").onclick = function() {
+        document.getElementById("row-adder-bar").style.display="none";
+        document.getElementById("add-row").style.display="block";
+    };
 
-    $("#add-liner-btn").click(function() {
-        chartnew = new HinocChart("line", $("#charts-container")[0]);
-        chartnew.show();
-    });
+    var hinocChartCounter = 1;
+    var arr = [];
+    document.getElementById("add-liner-btn").onclick = function() {
+        let chartContainer = document.getElementById("charts-container");
+        arr.push(<HinocChartReact key="{hinocChartCounter}" />);
+        hinocChartCounter ++;
+        ReactDOM.render(
+            arr,
+            chartContainer,
+            function() {hinocChartCounter++;}
+        );
+    };
 
-    $("#add-bar-btn").click(function() {
-        chartnew = new HinocChart("bar", $("#charts-container")[0]);
-        chartnew.show();
-    });
+    document.getElementById("add-bar-btn").onclick = function() {
 
-    $("#add-pan-btn").click(function() {
-        chartnew = new HinocChart("pan", $("#charts-container")[0]);
-        chartnew.show();
-    });
-});
+    };
+
+    document.getElementById("add-pan-btn").onclick = function() {
+    };
+},false);
--- app/static/js/main/hinocchart.js
+++ app/static/js/main/hinocchart.js
@@ -4,6 +4,7 @@ function HinocChart(chartType, container) {
     }
     this.chartContainer = $('<div class="col-md-12" style="height:400px;"></div>')[0];
     this.wholeContainer = $('<div class="col-md-12" style="height:420px;"></div>');
+    this.wholeContainer.append('<>')
     container.append(this.chartContainer);
     this.chart = echarts.init(this.chartContainer);
     this.chartoption = {
